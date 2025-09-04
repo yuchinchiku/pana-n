bd7f27b3836703ef211243de9067563233db6eb9
yutakahashi <yuu.takahashi@idealump.com>
2025-09-04 12:24:39 +0900

    menu done

--- src/data/storeDetails.ts
+++ src/data/storeDetails.ts
@@ -197,6 +197,76 @@ export const storeDetails: StoreDetail[] = [
       title: "【改定のお知らせ】",
       desc: "2025年7月1日よりメニュー及び価格の見直しをさせていただきます。",
     },
+    menu: [
+      {
+        name: "頭爽快の秘技「頭のもみほぐしコース」",
+        description: [
+          "頭頂部～側頭部、首・肩をしっかりマッサージすることで深い眠りに誘います。",
+          "パソコン・スマホの疲れが溜まっている方、睡眠不足でお悩みの方へおすすめです。満足度100％に自信有り。",
+          "極上な頭の秘技マッサージを是非ご勘能ください。",
+        ],
+        image: "img_head-massage2",
+        options: [
+          {
+            items: [
+              { duration: "40分", price: "XXX円（税込XXX円）" },
+              { duration: "60分", price: "XXX円（税込XXX円）" },
+            ],
+          },
+        ],
+      },
+      {
+        name: "歩き疲れた足を軽やかに「足ツボほぐしコース」",
+        description: [
+          "膝下～足裏までしっかりとほぐしマッサージします。",
+          "足ツボを押すことで全身の筋肉が緩み、疲れた足を軽やかに癒します。",
+        ],
+        image: "img_ashitsubo",
+        options: [
+          {
+            items: [
+              { duration: "40分", price: "XXX円（税込XXX円）" },
+              { duration: "60分", price: "XXX円（税込XXX円）" },
+            ],
+          },
+        ],
+      },
+      {
+        name: "究極こねり技「全身もみほぐしコース」",
+        description: [
+          "筋肉をこねりながらマッサージすることにより、つらいコリもすばやくほぐれます。",
+          "ほぐれた状態が通常より長く持続し、マッサージも快適にお過ごしいただけます。",
+        ],
+        image: "img_momihogushi",
+        options: [
+          {
+            items: [
+              { duration: "40分", price: "XXX円（税込XXX円）" },
+              { duration: "60分", price: "XXX円（税込XXX円）" },
+              { duration: "80分", price: "XXX円（税込XXX円）" },
+              { duration: "100分", price: "XXX円（税込XXX円）" },
+            ],
+          },
+        ],
+      },
+      {
+        name: "満足度No1「全身＆頭のもみほぐしコース」",
+        description: [
+          "お客様満足度No1マッサージメニューです。",
+          "100分以上のロングマッサージコースは特にオススメ！至極の癒しを是非ご体感ください。",
+        ],
+        image: "img_fullmassage",
+        options: [
+          {
+            items: [
+              { duration: "60分", price: "XXX円（税込XXX円）" },
+              { duration: "80分", price: "XXX円（税込XXX円）" },
+              { duration: "100分", price: "XXX円（税込XXX円）" },
+            ],
+          },
+        ],
+      },
+    ],
     gallery: [
       { src: "gallery-1.jpg", type: "portrait", className: "u-gallery-1 md:mr-20 md:ml-10" },
       { src: "gallery-2.jpg", type: "landscape", className: "u-gallery-2 md:mt-60 md:mr-0 md:ml-auto" },
@@ -229,6 +299,60 @@ export const storeDetails: StoreDetail[] = [
       { label: "アクセス", items: [{ value: "大森駅から徒歩2分（140m）" }] },
       { label: "お支払い方法", items: [{ value: "現金 / 各クレジットカード / 各電子マネー" }] },
     ],
+    menu: [
+      {
+        name: "頭爽快の秘技「頭のもみほぐしコース」",
+        description: [
+          "頭頂部～側頭部、首・肩をしっかりマッサージすることで深い眠りに誘います。",
+          "パソコン・スマホの疲れが溜まっている方、睡眠不足でお悩みの方へおすすめです。満足度100％に自信有り。",
+          "極上な頭の秘技マッサージを是非ご勘能ください。",
+        ],
+        image: "img_head-massage2",
+        options: [
+          {
+            items: [
+              { duration: "40分", price: "XXX円（税込XXX円）" },
+              { duration: "60分", price: "XXX円（税込XXX円）" },
+            ],
+          },
+        ],
+      },
+      {
+        name: "究極こねり技「全身もみほぐしコース」",
+        description: [
+          "筋肉をこねりながらマッサージすることにより、つらいコリもすばやくほぐれます。",
+          "ほぐれた状態が通常より長く持続し、マッサージも快適にお過ごしいただけます。",
+        ],
+        image: "img_momihogushi",
+        options: [
+          {
+            items: [
+              { duration: "40分", price: "XXX円（税込XXX円）" },
+              { duration: "60分", price: "XXX円（税込XXX円）" },
+              { duration: "80分", price: "XXX円（税込XXX円）" },
+              { duration: "100分", price: "XXX円（税込XXX円）" },
+            ],
+          },
+        ],
+      },
+      {
+        name: "満足度No1「全身＆頭のもみほぐしコース」",
+        description: [
+          "お客様満足度No1マッサージメニューです。",
+          "100分以上のロングマッサージコースは特にオススメ！至極の癒しを是非ご体感ください。",
+        ],
+        image: "img_fullmassage",
+        options: [
+          {
+            items: [
+              { duration: "60分", price: "XXX円（税込XXX円）" },
+              { duration: "80分", price: "XXX円（税込XXX円）" },
+              { duration: "100分", price: "XXX円（税込XXX円）" },
+            ],
+          },
+        ],
+      },
+    ],
   },
 
   //=====星のや沖縄スパ（星野リゾート星のや沖縄）===============================
@@ -254,6 +378,163 @@ export const storeDetails: StoreDetail[] = [
       { label: "お支払い方法", items: [{ value: "現金 / 各クレジットカード / 各電子マネー" }] },
       { label: "ご予約詳細", items: [{ value: "<a href='https://hoshinoresorts.com/ja/hotels/hoshinoyaokinawa/activities/13933/' target='_blank' class='u-link-blank underline'>星のや沖縄スパ</a>" }] },
     ],
+    menu: [
+      {
+        name: "スパメニュー「波　NAMI」",
+        description: [
+          "＜背中・脚・腕・デコルテ・頭皮＞",
+          "水溶性のトリートメントジェルや多くの美容成分を含むオイルでお顔をのぞく全身の肌を整えながら、リラックスへと導くボディートリートメントです。",
+        ],
+        image: "img_hoshinoOki-spa",
+        options: [
+          {
+            items: [
+              { duration: "90分", price: "25,300円（税込）" },
+            ],
+          },
+        ],
+      },
+      {
+        name: "スパメニュー「陽　HARU」",
+        description: [
+          "＜背中・脚・腕・デコルテ・頭皮＞",
+          "やさしい月桃の香りに癒されながら、琉球空手と琉球舞踊の動きを用いた手技で、全身を緩めるボディートリートメントです。",
+        ],
+        image: "img_hoshinoOki-spa",
+        options: [
+          {
+            items: [
+              { duration: "90分", price: "25,300円（税込）" },
+            ],
+          },
+        ],
+      },
+      {
+        name: "スパメニュー「渚　NAGISA」",
+        description: [
+          "＜背中パック・脚・背中・腕・デコルテ・頭皮＞",
+          "お背中に発熱性の海藻パックを施し、お身体を温めながら行う全身のボディトリートメントです。",
+          "より深いリラックスへと導き、心地よいひとときをお過ごし頂けます。",
+        ],
+        image: "img_hoshinoOki-spa",
+        options: [
+          {
+            items: [
+              { duration: "90分", price: "31,196円（税込）" },
+            ],
+          },
+        ],
+      },
+      {
+        name: "スパメニュー「汐　SHIO」",
+        description: [
+          "＜背中パック・脚・背中・腕・デコルテ・頭皮＞",
+          "お背中に海泥ミネラルパックを施しながら行う、全身のボディトリートメントです。",
+          "お肌を滑らかに整えたのちにリズミカルなストロークで軽快に仕上げます。",
+        ],
+        image: "img_hoshinoOki-spa",
+        options: [
+          {
+            items: [
+              { duration: "90分", price: "31,196円（税込）" },
+            ],
+          },
+        ],
+      },
+      {
+        name: "スパメニュー「月　TSUKI」",
+        description: [
+          "＜背中・脚・腕・デコルテ・頭皮＞ ",
+          "沖縄の代表的なハーブ「月桃」をもち米粉と共に練って蒸しあげた「月桃玉」をお身体にあてながら行う全身のボディトリートメントです。",
+          "温かく香りのよい「月桃玉」で、全身を心地よく和らげてまいります。",
+          "※開始一時間前までにご予約をお願いします。",
+        ],
+        image: "img_hoshinoOki-spa",
+        options: [
+          {
+            items: [
+              { duration: "90分", price: "31,196円（税込）" },
+            ],
+          },
+        ],
+      },
+      {
+        name: "スパメニュー「凪　NAGI」",
+        description: [
+          "＜背中・脚・腕・デコルテ・頭皮・顔（クレンジング＋オイルトリートメント）＞ ",
+          "月桃エキスやアロエエキスなどの美容成分とアルガンオイルで明るいお肌へと導きます。",
+          "全身のトリートメントとともに、深くリラックスしていただけるメニューです。",
+        ],
+        image: "img_hoshinoOki-spa",
+        options: [
+          {
+            items: [
+              { duration: "90分", price: "26,400円（税込）" },
+            ],
+          },
+        ],
+      },
+      {
+        name: "スパメニュー「雫　SHIZUKU」",
+        description: [
+          "＜背中・脚・腕・デコルテ・頭皮・顔（クレンジング＋オイルトリートメント＋マスク）＞ ",
+          "お背中から身体を丁寧にほぐし、お顔には月桃ローションとコラーゲンマスクを行い、アルガンオイルでふっくらとお肌を仕上げる、全身のトリートメントです。",
+        ],
+        image: "img_hoshinoOki-spa",
+        options: [
+          {
+            items: [
+              { duration: "90分", price: "32,670円（税込）" },
+            ],
+          },
+        ],
+      },
+      {
+        name: "日本の伝統手技「指圧マッサージ」",
+        description: [
+          "日本の伝統的な指圧に、琉球王家に伝わる空手と舞踊の型を組み合わせた、特徴的な手技による指圧マッサージです。",
+          "※客室のみになります。",
+        ],
+        image: "img_hoshinoOki-hogushi",
+        options: [
+          {
+            items: [
+              { duration: "60分", price: "14,520円（税込）" },
+            ],
+          },
+        ],
+      },
+      {
+        name: "日本の伝統手技「マタニティ－マッサージ」",
+        description: [
+          "ご妊娠中のお身体の疲れを、着衣のままで丁寧にほぐすマッサージメニューです。",
+          "16週目以降、27週目未満で経過順調な方は、お受けいただけます。",
+        ],
+        image: "img_hoshinoOki-hogushi",
+        options: [
+          {
+            items: [
+              { duration: "90分", price: "22,990円（税込）" },
+            ],
+          },
+        ],
+      },
+      {
+        name: "日本の伝統手技「癒　YU」",
+        description: [
+          "東洋医学に精通した施術師が、東洋医学の伝統的な問診や視診、脈診に基づき、お客様の体質・体調に合わせた鍼または灸による施術で、人間本来の自然治癒力を引き出し、本来の身体の調子へと導きます。",
+          "※こちらのメニューは日本語でのご案内のみです。",
+        ],
+        image: "img_hoshinoOki-hogushi",
+        options: [
+          {
+            items: [
+              { duration: "150分", price: "42,350円（税込）" },
+            ],
+          },
+        ],
+      },
+    ]
   },
 
   //=====星のや竹富島スパ（星野リゾート星のや竹富島）===============================
@@ -276,6 +557,107 @@ export const storeDetails: StoreDetail[] = [
       { label: "営業時間", items: [{ value: "9:00 AM～17:30 PM<br>※最終受付 15:00 PM" }] },
       { label: "お支払い方法", items: [{ value: "現金 / 各クレジットカード / 各電子マネー" }] },
       { label: "ご予約詳細", items: [{ value: "<a href='https://hoshinoresorts.com/ja/hotels/hoshinoyataketomijima/activities/13939/' target='_blank' class='u-link-blank underline'>星のや竹富島スパ</a>" }] },
+    ],
+    menu: [
+      {
+        name: "特別メニュー「海の恵み いんぬふぁーすん」",
+        description: [
+          "＜背中（海藻パック）、脚、腕、デコルテ、ヘッド＞",
+          "健康の宝として重宝されてきた海の恵みである海藻を使用したメニューです。",
+          "背中に発熱性の海藻パックを施すことでお身体を温め、同時に乾燥しがちな背中にも潤いを与えます。",
+          "＊甲状腺トラブル、ヨードアレルギーをお持ちのお客様はご遠慮いただいております。",
+        ],
+        image: "img_taketomi-sp",
+        options: [
+          {
+            items: [
+              { duration: "90分（所要時間 120 分）", price: "30,500円（税込）" },
+            ],
+          },
+        ],
+      },
+      {
+        name: "特別メニュー「海の潤い いんぬうるい」",
+        description: [
+          "＜背中・脚・デコルテ・顔（海藻マスク）・ヘッド・足浴＞",
+          "八重山の珊瑚礁に囲まれた島々は、海藻の宝庫と言われています。",
+          "海藻成分をたっぷりと含んだマスクでお手入れすることで、お肌に潤いを与えて艶やかに整えます。",
+        ],
+        image: "img_taketomi-sp",
+        options: [
+          {
+            items: [
+              { duration: "90分（所要時間 120 分）", price: "30,500円（税込）" },
+            ],
+          },
+        ],
+      },
+      {
+        name: "特別メニュー「命ぐすい草の休息 ぶがりのーすん」",
+        description: [
+          "＜背中・脚・腕・デコルテ・頭皮＞",
+          "長命草やフーチバー(蓬)を使用した「命ぐすい玉」でお身体を温めるメニューです。",
+          "命の薬として島で重宝されてきた命ぐすいで安らぎの休息へと誘います。",
+          "＊準備にお時間がかかりますため、前日までのご予約をお願いいたします。",
+        ],
+        image: "img_taketomi-sp",
+        options: [
+          {
+            items: [
+              { duration: "90分（所要時間 120 分）", price: "30,500円（税込）" },
+            ],
+          },
+        ],
+      },
+      {
+        name: "スタンダードメニュー「休 ゆくい」",
+        description: [
+          "＜背中・脚・腕・デコルテ・ヘッド・足浴＞",
+          "離島のゆっくりと流れる島時間に身をゆだね、深いリラックスへと誘うスパのスタンダードメニューです。",
+          "お顔をのぞく全身をトリートメントし、珊瑚礁の砂浜に静かに打ち寄せては返す波のようなリズムで頭皮をほぐし、心地よい安らぎへと導きます。",
+        ],
+        image: "img_taketomi-s",
+        options: [
+          {
+            items: [
+              { duration: "90分（所要時間 120 分）", price: "23,230円（税込）" },
+            ],
+          },
+        ],
+      },
+      {
+        name: "スタンダードメニュー「養 ひかのーん」",
+        description: [
+          "＜背中・脚・腕・デコルテ・ヘッド・足浴＞",
+          "お背中、お肩周りを特に丁寧にトリートメントするメニューです。",
+          "お好みの香りに包まれながら全身をトリートメントし、ゆったりとした波のようなリズムで頭皮をほぐし深いリラックスへと誘います。",
+        ],
+        image: "img_taketomi-s",
+        options: [
+          {
+            items: [
+              { duration: "90分（所要時間 120 分）", price: "23,230円（税込）" },
+            ],
+          },
+        ],
+      },
+      {
+        name: "スタンダードメニュー「美 あぱれさん 」",
+        description: [
+          "＜背中・脚・デコルテ・顔・ヘッド・足浴＞",
+          "お顔を含む全身のトリートメントです。",
+          "アロマの爽やかな香りに包まれて、ゆったりとした波のようなリズムでトリートメントを行います。",
+          "お顔は丁寧にクレンジングした後にオイルトリートメントを行い、心地よいリラックスと透明感のあるお肌へと導きます。",
+        ],
+        image: "img_taketomi-s",
+        options: [
+          {
+            items: [
+              { duration: "90分（所要時間 120 分）", price: "23,230円（税込）" },
+            ],
+          },
+        ],
+      },
     ],
     gallery: [
       { src: "gallery-1.jpg", type: "portrait", className: "u-gallery-1 md:mr-20 md:ml-10" },
@@ -306,6 +688,71 @@ export const storeDetails: StoreDetail[] = [
       { label: "お支払い方法", items: [{ value: "現金 / 各クレジットカード / 各電子マネー" }] },
       { label: "ご予約詳細", items: [{ value: "<a href='https://hoshinoresorts.com/ja/hotels/iriomote/sp/iriomotespa_/' target='_blank' class='u-link-blank underline'>西表スパ</a>" }] },
     ],
+    menu: [
+      {
+        name: "イリムティチヒカラ（西表パワー）",
+        description: [
+          "＜ジェルボディトリートメント＞",
+          "オーガニック植物エキスをたっぷりと含んだ水溶性ジェルでお身体全体をゆったりとほぐし、深いリラクゼーションへと導きます。",
+        ],
+        image: "img_iriomote-body",
+        options: [
+          {
+            items: [
+              { duration: "60分", price: "15,895円（税込）" },
+            ],
+          },
+        ],
+      },
+      {
+        name: "ガンジュー（元気）",
+        description: [
+          "＜シークワーサー足浴+フットケア＆ヘッドトリートメント＆ハンドトリートメント＞",
+          "シークワーサーエキスを含む足浴でリラックスした後、美容成分を多く含む水溶性ジェルで膝下から足裏まで、肘から指先、疲労の溜まりやすい肩から頭部をスッキリとほぐします。",
+          "※着衣で行います。",
+        ],
+        image: "img_iriomote-head",
+        options: [
+          {
+            items: [
+              { duration: "60分", price: "15,895円（税込）" },
+            ],
+          },
+        ],
+      },
+      {
+        name: "ティンヌカーラ（天の川）",
+        description: [
+          "＜シークワーサー足浴＆ジェルボディトリートメント＆ヘッドトリートメント＞",
+          "シークワーサーエキスを含む足浴の後、植物由来の美容成分を含むジェルでお身体全体をゆったりとほぐします。",
+          "リラックス効果の高い ヘッドトリートメントと併せて行うことで、よりいっそう深いリラクゼーションへと導きます。",
+        ],
+        image: "img_iriomote-full",
+        options: [
+          {
+            items: [
+              { duration: "90分", price: "18,700円（税込）" },
+            ],
+          },
+        ],
+      },
+      {
+        name: "ピッカル(輝く)",
+        description: [
+          "＜シークワーサー足浴+ジェルボディトリートメント＆フェイシャルトリートメント＋マスク＆ヘッドトリートメント＞",
+          "美容成分を多く含む水溶性ジェルでお身体を丁寧にほぐした後に、月桃エキスなどを含む化粧品でお顔の肌を整え、シークワーサー特有の美容成分ノビレチンを含んだマスクで透明感のある肌に仕上げます。",
+          "ヘッドトリートメントを併せることで、顔色がいっそう明るく整います。",
+        ],
+        image: "img_iriomote-full",
+        options: [
+          {
+            items: [
+              { duration: "90分", price: "21,000円（税込）" },
+            ],
+          },
+        ],
+      },
+    ],
     gallery: [
       { src: "gallery-1.jpg", type: "portrait", className: "u-gallery-1 md:mr-20 md:ml-10" },
       { src: "gallery-2.jpg", type: "landscape", className: "u-gallery-2 md:mt-60 md:mr-0 md:ml-auto" },
@@ -335,6 +782,55 @@ export const storeDetails: StoreDetail[] = [
       { label: "お支払い方法", items: [{ value: "現金 / 各クレジットカード / 各電子マネー" }] },
       { label: "ご予約詳細", items: [{ value: "<a href='https://hoshinoresorts.com/ja/hotels/risonarekohamajima/sp/newspa/' target='_blank' class='u-link-blank underline'>リゾナーレ小浜島スパ</a>" }] },
     ],
+    menu: [
+      {
+        name: "天空(てぃん)",
+        description: [
+          "＜ハイビスカス足浴＋ジェルボディトリートメント＆ヘッドトリートメント＞",
+          "オーガニック植物抽出エキスを含んだトリートメントジェルで、全身を丁寧にほぐします。",
+          "やさしい波のようなリズムで頭皮をやわらげ、深いリラックスへと導きます。",
+        ],
+        image: "img_kohama-body",
+        options: [
+          {
+            items: [
+              { duration: "90分", price: "21,500円（税込）" },
+            ],
+          },
+        ],
+      },
+      {
+        name: "珊瑚礁（ぴー）",
+        description: [
+          "＜ハイビスカス足浴＋ジェルボディトリートメント＆ボディパック又はボディスクラブ＞",
+          "沖縄天然素材のハイビスカスやシークヮーサーなどのエキスを含むボディパックまたはスクラブで背中の肌を整えてから、美容成分を多く含む水溶性ジェルで全身を緩めるボディトリートメントです。",
+        ],
+        image: "img_kohama-body",
+        options: [
+          {
+            items: [
+              { duration: "90分", price: "24,300円（税込）" },
+            ],
+          },
+        ],
+      },
+      {
+        name: "祈(にんがい)",
+        description: [
+          "＜ハイビスカス足浴＋ジェルボディトリートメント＆ヘッドトリートメント＆フットトリートメント＞",
+          "全身と頭をトリートメントし、お疲れの足をていねいにほぐします。",
+          "小浜島のやさしい空気に身をゆだね深いリラックスへと誘います。",
+        ],
+        image: "img_kohama-full",
+        options: [
+          {
+            items: [
+              { duration: "90分", price: "21,500円（税込）" },
+            ],
+          },
+        ],
+      },
+    ],
     gallery: [
       { src: "gallery-1.jpg", type: "portrait", className: "u-gallery-1 md:mr-20 md:ml-10" },
       { src: "gallery-2.jpg", type: "landscape", className: "u-gallery-2 md:mt-60 md:mr-0 md:ml-auto" },
@@ -363,7 +859,25 @@ export const storeDetails: StoreDetail[] = [
       { label: "電話番号", items: [{ value: "090-9785-1838" }] },
       { label: "営業時間", items: [{ value: "11:00 PM～19:30 PM<br>※最終受付 18:00 PM" }] },
       { label: "お支払い方法", items: [{ value: "現金 / 各クレジットカード / 各電子マネー" }] },
-      { label: "ご予約詳細", items: [{ value: "<a href='https://www.fusaki.com/spa_wellness' target='_blank' class='u-link-blank underline'>星のや沖縄</a>" }] },
+      { label: "ご予約詳細", items: [{ value: "<a href='https://www.fusaki.com/spa_wellness' target='_blank' class='u-link-blank underline'>フサキビーチリゾートホテル＆ヴィラズ</a>" }] },
     ],
+    // menu: [
+    //   {
+    //     name: "天空(てぃん)",
+    //     description: [
+    //       "＜ハイビスカス足浴＋ジェルボディトリートメント＆ヘッドトリートメント＞",
+    //       "オーガニック植物抽出エキスを含んだトリートメントジェルで、全身を丁寧にほぐします。",
+    //       "やさしい波のようなリズムで頭皮をやわらげ、深いリラックスへと導きます。",
+    //     ],
+    //     image: "img_kohama-body",
+    //     options: [
+    //       {
+    //         items: [
+    //           { duration: "90分", price: "21,500円（税込）" },
+    //         ],
+    //       },
+    //     ],
+    //   },
+    // ]
   },
 ];
