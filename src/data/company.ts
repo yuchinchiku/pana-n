6d67a74ce6206ea46de099b8790df8a82a6eac5a
yutakahashi <yuu.takahashi@idealump.com>
2025-09-05 07:08:53 +0900

    マッサージ文言修正

--- src/data/company.ts
+++ src/data/company.ts
@@ -13,7 +13,7 @@ export const company: Company = {
     { label: "E-mail", items: [{ value: "<a class='u-link-blank underline' href='email:spa@pana-n.jp'>spa@pana-n.jp</a>" }] },
     { label: "WEBサイト", items: [{ value: "<a class='u-link underline' href='email:spa@pana-n.jp'>https://pana-n.jp</a>" }] },
     { label: "設立", items: [{ value: "	2006年3月8日" }] },
-    { label: "事業内容", items: [{ value: "マッサージ・エステ・スパ等の店舗経営及び業務受託運営<br>マッサージ・エステ・スパ等の療法士育成及び企画コンサルタント" }] },
+    { label: "事業内容", items: [{ value: "ほぐし・エステ・スパ等の店舗経営及び業務受託運営<br>ほぐし・エステ・スパ等の療法士育成及び企画コンサルタント" }] },
     { label: "所在地", items: [{ value: "〒907-0242 沖縄県石垣市白保1960−104−1 石垣空港2F", mapUrl: 'https://maps.app.goo.gl/BgVgCvhpBUMSgC5r9' }] },
   ],
 };
